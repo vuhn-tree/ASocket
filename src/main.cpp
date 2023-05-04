aee5c05f73ce50ac5f0b9ba433272f0517b73dc8
Richard Vu <rqvu.mail@gmail.com>
2023-05-04 01:05:37 -0700

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,3 @@
-// #include <Arduino.h>
-
-// void setup() {
-//   // put your setup code here, to run once:
-// }
-
-// void loop() {
-//   // put your main code here, to run repeatedly:
-// }
-
-
 #include <M5Atom.h>
 #include <Adafruit_NeoPixel.h>
 
