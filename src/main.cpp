740f11dfd34c2cf7c6b8f29fd86aa13557f1fb73
Richard Vu <rqvu.mail@gmail.com>
2023-05-04 01:04:13 -0700

    initial led test

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,59 @@
-#include <Arduino.h>
+// #include <Arduino.h>
 
+// void setup() {
+//   // put your setup code here, to run once:
+// }
+
+// void loop() {
+//   // put your main code here, to run repeatedly:
+// }
+
+
+#include <M5Atom.h>
+#include <Adafruit_NeoPixel.h>
+
+#define PIN       27  
+#define NUMPIXELS 1   
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(
+    NUMPIXELS, PIN,
+    NEO_GRB + NEO_KHZ800);  // set number of LEDs, pin number, LED type.
+                            
+                         
 void setup() {
-  // put your setup code here, to run once:
+    M5.begin();      // Init M5Atom
+    pixels.begin();  // Init the NeoPixel library
+
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return pixels.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return pixels.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return pixels.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
+
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+    for(i=0; i<pixels.numPixels(); i++) {
+      pixels.setPixelColor(i, Wheel((i+j) & 255));
+      while(M5.Btn.read()== 1);
+    }
+    pixels.show();
+    delay(wait);
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    rainbow(20);
 }
