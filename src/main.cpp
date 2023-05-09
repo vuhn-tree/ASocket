9cf72a0b3aa02be2cb5329607b62bfe502d7dba4
Richard Vu <rqvu.mail@gmail.com>
2023-05-08 18:33:30 -0700

    revert

--- src/main.cpp
+++ src/main.cpp
@@ -15,17 +15,17 @@ void setup() {
 
 // Input a value 0 to 255 to get a color value.
 // The colours are a transition r - g - b - back to r.
-uint32_t Wheel(byte rainbowPos) {
-  rainbowPos = 255 - rainbowPos;
-  if (rainbowPos < 85) {
-    return pixels.Color(255 - rainbowPos * 3, 0, rainbowPos * 3);
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if (WheelPos < 85) {
+    return pixels.Color(255 - WheelPos * 3, 0, WheelPos * 3);
   }
-  if (rainbowPos < 170) {
-    rainbowPos -= 85;
-    return pixels.Color(0, rainbowPos * 3, 255 - rainbowPos * 3);
+  if (WheelPos < 170) {
+    WheelPos -= 85;
+    return pixels.Color(0, WheelPos * 3, 255 - WheelPos * 3);
   }
-  rainbowPos -= 170;
-  return pixels.Color(rainbowPos * 3, 255 - rainbowPos * 3, 0);
+  WheelPos -= 170;
+  return pixels.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
 }
 
 void rainbow(uint8_t wait) {
