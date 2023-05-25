5ae2f9b99d5043322c67ea9196e0ec9ff89ecef7
Richard Vu <rqvu.mail@gmail.com>
2023-05-24 23:05:46 -0700

    build fixes

--- src/main.cpp
+++ src/main.cpp
@@ -48,35 +48,35 @@ void setup() {
   pixels.begin();  // Init the NeoPixel library
 }
 
-// Input a value 0 to 255 to get a color value.
-// The colours are a transition r - g - b - back to r.
-uint32_t Wheel(byte WheelPos) {
-  WheelPos = 255 - WheelPos;
-  if (WheelPos < 85) {
-    return pixels.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  }
-  if (WheelPos < 170) {
-    WheelPos -= 85;
-    return pixels.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  }
-  WheelPos -= 170;
-  return pixels.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-}
+// // Input a value 0 to 255 to get a color value.
+// // The colours are a transition r - g - b - back to r.
+// uint32_t Wheel(byte WheelPos) {
+//   WheelPos = 255 - WheelPos;
+//   if (WheelPos < 85) {
+//     return pixels.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+//   }
+//   if (WheelPos < 170) {
+//     WheelPos -= 85;
+//     return pixels.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+//   }
+//   WheelPos -= 170;
+//   return pixels.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+// }
 
-void rainbow(uint8_t wait) {
-  uint16_t i, j;
+// void rainbow(uint8_t wait) {
+//   uint16_t i, j;
 
-  for (j = 0; j < 256; j++) {
-    for (i = 0; i < pixels.numPixels(); i++) {
-      pixels.setPixelColor(i, Wheel((i + j) & 255));
-      while (M5.Btn.read() == 1)
-        ;
-    }
-    pixels.show();
-    delay(wait);
-  }
-}
+//   for (j = 0; j < 256; j++) {
+//     for (i = 0; i < pixels.numPixels(); i++) {
+//       pixels.setPixelColor(i, Wheel((i + j) & 255));
+//       while (M5.Btn.read() == 1)
+//         ;
+//     }
+//     pixels.show();
+//     delay(wait);
+//   }
+// }
 
 void loop() { 
-  rainbow(20); 
+  // rainbow(20); 
 }
